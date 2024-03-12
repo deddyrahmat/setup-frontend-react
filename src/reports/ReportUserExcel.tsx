aef961cb7d4d5e05922175e36a3b628479a5b86c
deddyrahmat <deddyrahmat97@gmail.com>
2024-03-13 04:59:45 +0700

    update style excel
    update style for report excel with xlsx-js-style

--- src/reports/ReportUserExcel.tsx
+++ src/reports/ReportUserExcel.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-import * as XLSX from "xlsx";
+import * as XLSX from "xlsx-js-style";
 
 function ReportUserExcel({ data }: any) {
   const handleDownload = () => {
-    let manipulate: any = [["User ID", "Name", "Email", "Role ID"]];
+    let manipulate: any = [
+      ["User ID", "Name", "Email", "Role ID"],
+      [" ", " ", " ", " "],
+    ];
     data.map((item: any) => {
       manipulate = [
         ...manipulate,
@@ -13,7 +16,60 @@ function ReportUserExcel({ data }: any) {
 
     // create new workbook
     const wb = XLSX.utils.book_new();
+
     const ws = XLSX.utils.aoa_to_sheet(manipulate);
+    const colWidths = [{ wch: 25 }, { wch: 25 }, { wch: 25 }, { wch: 20 }];
+    // console.log("ws", ws);
+    ws["A1"].s = {
+      font: {
+        bold: true,
+        // color: { rgb: "FFAA00" },
+      },
+      // fill: {
+      //   bgColor: { rgb: "FF0269D8" }, // ARGB Hex Value
+      //   fgColor: { rgb: "FF0269D8" }, // ARGB Hex Value
+      // },
+      alignment: {
+        horizontal: "center",
+        vertical: "center",
+      },
+    };
+    ws["B1"].s = {
+      font: {
+        bold: true,
+      },
+      alignment: {
+        horizontal: "center",
+        vertical: "center",
+      },
+    };
+    ws["C1"].s = {
+      font: {
+        bold: true,
+      },
+      alignment: {
+        horizontal: "center",
+        vertical: "center",
+      },
+    };
+    ws["D1"].s = {
+      font: {
+        bold: true,
+      },
+      alignment: {
+        horizontal: "center",
+        vertical: "center",
+      },
+    };
+
+    ws["!cols"] = colWidths;
+    const merge = [
+      { s: { r: 0, c: 0 }, e: { r: 1, c: 0 } },
+      { s: { r: 0, c: 1 }, e: { r: 1, c: 1 } },
+      { s: { r: 0, c: 2 }, e: { r: 1, c: 2 } },
+      { s: { r: 0, c: 3 }, e: { r: 1, c: 3 } },
+    ];
+    ws["!merges"] = merge;
 
     // add worksheet to workbook
     XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
